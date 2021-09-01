a74291e078a11e7b3b883fce6f6864a90019ff57
Maycon Silva (HP) <jose.maycon.hermogenes.silva@gmail.com>
2021-09-01 19:59:56 -0300

    ecosystem.config.js | Configuração base

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,20 +5,24 @@ module.exports = {
         script: "./src/server/index.js",
         watch: true,
         env: {
-            "PORT": 8888,
-            "MONGO_URL":  "mongodb+srv://user:user@db.wvjag.mongodb.net/db?retryWrites=true&w=majority",
-            "WORD_SECRET": "feiraapp-develompent",
-            "LIMIT_PAGINATION": 10,
-            "SERVICE_EMAIL_TOKEN": "3334E5D751CA7D5CEB5E8511DA38FFAA9B18E74692204112E686EA63ADD5F5DF2BD766DD8725D9AB6EB588D71F57A9E88C3EAB0CCDD21B9806E40E75631638A2",
-            "NODE_ENV": "development"
+            'PORT': 8888,
+            'MONGO_URL':  'mongodb+srv://feiraappadm:feiraappadm@feira-app.90cyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+            'WORD_SECRET': 'feiraapp-develompent',
+            'LIMIT_PAGINATION': 20,
+            'SERVICE_EMAIL_TOKEN': '3334E5D751CA7D5CEB5E8511DA38FFAA9B18E74692204112E686EA63ADD5F5DF2BD766DD8725D9AB6EB588D71F57A9E88C3EAB0CCDD21B9806E40E75631638A2',
+            'NODE_ENV': 'development',
+            'SECRET_KEY': '3334E5D751CA7D5CEB5E8511DA38FFAA9B18E74692204112E686EA63ADD5F5DF2BD766DD8725D9AB6EB588D71F57A9E88C3EAB0CCDD21B9806E40E75631638A2',
+            'AUTHENTICATION_WORD': ''
         },
         env_production: {
-          "PORT": 3000,
-          "MONGO_URL":  "mongodb+srv://mentalpro:serrambi@qad.x9cj0.mongodb.net/qad?retryWrites=true&w=majority",
-          "WORD_SECRET": "feiraapp-avaliable",
-          "LIMIT_PAGINATION": 10,
-          "SERVICE_EMAIL_TOKEN": "3334E5D751CA7D5CEB5E8511DA38FFAA9B18E74692204112E686EA63ADD5F5DF2BD766DD8725D9AB6EB588D71F57A9E88C3EAB0CCDD21B9806E40E75631638A2",
-          "NODE_ENV": "production"
+          'PORT': 3000,
+          'MONGO_URL':  'mongodb+srv://feiraappadm:feiraappadm@feira-app.90cyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+          'WORD_SECRET': 'feiraapp-avaliable',
+          'LIMIT_PAGINATION': 20,
+          'SERVICE_EMAIL_TOKEN': '3334E5D751CA7D5CEB5E8511DA38FFAA9B18E74692204112E686EA63ADD5F5DF2BD766DD8725D9AB6EB588D71F57A9E88C3EAB0CCDD21B9806E40E75631638A2',
+          'NODE_ENV': 'production',
+          'SECRET_KEY': '3334E5D751CA7D5CEB5E8511DA38FFAA9B18E74692204112E686EA63ADD5F5DF2BD766DD8725D9AB6EB588D71F57A9E88C3EAB0CCDD21B9806E40E75631638A2',
+          'AUTHENTICATION_WORD': ''
         }
       }
   ]
